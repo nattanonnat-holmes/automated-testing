45b78ec80c1d63f6a29ac41b04995120c2cabac3
nattanonnat <boiethai.society@gmail.com>
2026-09-23 00:43:59 +0700

    test: expand E2E coverage with tags and persistence

--- tests/e2e/todo.spec.ts
+++ tests/e2e/todo.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '../../src/fixtures/testFixture';
 import { generateUniqueTitle } from '../../src/utils/testHelpers';
 
 test.describe('TodoMVC - End-to-End Test Suite', () => {
-  test('should allow adding new items to the todo list', async ({ todoPage }) => {
+  test('@smoke should allow adding new items to the todo list', async ({ todoPage }) => {
     const item1 = generateUniqueTitle('todo');
     const item2 = generateUniqueTitle('todo');
 
@@ -86,7 +86,18 @@ test.describe('TodoMVC - End-to-End Test Suite', () => {
 
     await todoPage.toggleAll();
 
-    await expect(todoPage.todoItems).toHaveClass(/completed/);
     await expect(todoPage.todoCount).toContainText('0 items left');
+    await expect(todoPage.todoItems.nth(0)).toHaveClass(/completed/);
+    await expect(todoPage.todoItems.nth(1)).toHaveClass(/completed/);
+    await expect(todoPage.todoItems.nth(2)).toHaveClass(/completed/);
+  });
+
+  test('should preserve todos after page reload', async ({ todoPage, page }) => {
+    const task = generateUniqueTitle('persist');
+
+    await todoPage.addTodo(task);
+    await page.reload();
+
+    await expect(todoPage.todoTitles).toHaveText([task]);
   });
 });
